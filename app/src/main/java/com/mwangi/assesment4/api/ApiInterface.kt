dc094e81aff33ad614cf6eeb470fd8d7a4d5fc5a
Wambui-Mwangi <mwangiestherwambuii@gmail.com>
2023-08-24 09:26:44 +0300

    fetching posts

--- app/src/main/java/com/mwangi/assesment4/api/ApiInterface.kt
+++ app/src/main/java/com/mwangi/assesment4/api/ApiInterface.kt
@@ -4,12 +4,10 @@ import com.mwangi.assesment4.model.Posts
 import com.mwangi.assesment4.model.PostsResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+
 
 interface ApiInterface {
         @GET("/posts")
         suspend fun getPosts(): Response<PostsResponse>
 
-//        @GET("/post/{id}")
-//        suspend fun getPostById(@Path("id")postID:Int): Response<Posts>
 }
